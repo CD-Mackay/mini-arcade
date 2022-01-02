fcf964e3549be5af715da6b938d9c321ae6f0d0a
Connor Mackay <connor.mackay@gmail.com>
2022-01-01 22:05:21 -0500

    build game list item component

--- src/components/atoms/GameListItem/StyledGameListItem.js
+++ src/components/atoms/GameListItem/StyledGameListItem.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
-const StyledGameListItem = styled.section`
-
+const StyledGameListItem = styled.div`
+display: flex;
+flex-direction: column;
 `
 
 export default StyledGameListItem;
