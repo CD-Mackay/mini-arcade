535430a3314d748b365acbb91580a815486053ca
Connor Mackay <connor.mackay@gmail.com>
2022-01-01 22:07:16 -0500

    Style gamelist item

--- src/components/atoms/GameListItem/StyledGameListItem.js
+++ src/components/atoms/GameListItem/StyledGameListItem.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
 
 const StyledGameListItem = styled.div`
+background: #A1B3D1;
+border-radius: 5px;
 display: flex;
 flex-direction: column;
+margin: 5px;
+padding: 5px;
+text-align: center;
 `
 
 export default StyledGameListItem;
