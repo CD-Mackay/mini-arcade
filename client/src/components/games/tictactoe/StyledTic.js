a4e70c2225d99c0b24eca769cca8bb1f50b86003
Connor Mackay <connor.mackay@gmail.com>
2022-01-05 17:32:20 -0500

    create openinig menu for tictactoe

--- client/src/components/games/tictactoe/StyledTic.js
+++ client/src/components/games/tictactoe/StyledTic.js
@@ -2,6 +2,30 @@ import styled from 'styled-components';
 
 const StyledTic = styled.section`
 
+#setup-box {
+  width: 40vw;
+  margin: 0 auto;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  justify-content: center;
+  h4 {
+    padding: 5px;
+    font-size: 42px;
+  }
+  #button-wrapper {
+    width: 60%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-around;
+    button {
+      width: 150px;
+      padding: 2px;
+
+    }
+  }
+}
 `
 
 export default StyledTic;
--- client/src/components/games/tictactoe/Tic.js
+++ client/src/components/games/tictactoe/Tic.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import StyledTic from './StyledTic';
 
 const Tic = () => {
+
+  const [gamePhase, setGamePhase] = useState("setup");
+
+
+  const startGame = () => {
+
+  }
   return (
     <StyledTic>
-      <p>I am Tic tac toe</p>
+      {gamePhase === "setup" &&
+      <div id="setup-box">
+        <h4>Select Human or Robotic Opponent</h4>
+        <div id="button-wrapper">
+          <button>human</button>
+          <button>robot</button>
+        </div>
+      </div>
+      }
+      {gamePhase === "play" && <div>
+        <h4>The Game is Afoot!</h4>
+      </div>}
     </StyledTic>
   )
 };
