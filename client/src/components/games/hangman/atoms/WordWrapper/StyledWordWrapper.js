fa2f81cce055bb1517b18cc1625f0bbf9fd2e795
Connor Mackay <connor.mackay@gmail.com>
2022-01-10 15:40:31 -0500

    style wordwrapper

--- client/src/components/games/hangman/atoms/WordWrapper/StyledWordWrapper.js
+++ client/src/components/games/hangman/atoms/WordWrapper/StyledWordWrapper.js
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const StyledWordWrapper = styled.div`
 display: flex;
 justify-content: space-between;
-width: 40vw;
+width: 50vw;
+margin-bottom: 10px;
 `
 
 export default StyledWordWrapper;
