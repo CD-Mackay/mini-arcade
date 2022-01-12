bd56b6841277998f915742eefef813294fccc243
Connor Mackay <connor.mackay@gmail.com>
2022-01-12 18:54:09 -0500

    Style gamelist, add descriptions

--- client/src/components/atoms/GameListItem/StyledGameListItem.js
+++ client/src/components/atoms/GameListItem/StyledGameListItem.js
@@ -1,19 +1,33 @@
 import styled from 'styled-components';
 
 const StyledGameListItem = styled.div`
-background: #A1B3D1;
+background: #24282f;
 border-radius: 5px;
+border: 1px solid #00FFFF;
 display: flex;
 flex-direction: column;
 margin: 5px;
-padding: 5px;
+padding: 10px;
 text-align: center;
-Link {
+a {
    text-decoration: none;
+   h4 {
+      color: #00FFFF;
+      font-size: 32px;
+      margin-bottom: 5px;
+   }
 }
-h4 {
-   color: black;
-   text-decoration: none;
+p {
+   color: #00FFFF;
+   font-size: 24px;
+}
+:hover {
+   h4 {
+      font-size: 33px;
+   }
+   p {
+      font-size: 25px;
+   }
 }
 `
 
