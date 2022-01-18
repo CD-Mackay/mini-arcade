1e54b41b3ae45a216f9029b2b4104ea23a30e6c7
Connor Mackay <connor.mackay@gmail.com>
2022-01-18 18:36:18 -0500

    Add letter validation

--- client/src/components/games/hangman/atoms/TextInput/TextInput.js
+++ client/src/components/games/hangman/atoms/TextInput/TextInput.js
@@ -8,10 +8,21 @@ import Button from '../../../../atoms/Button/Button';
 
 const TextInput = ({setInput, input, onSubmit}) => {
 
+  let regex = /[a-zA-Z]/;
+
+  const [error, setError] = useState(false);
+
   const handleSetInput = (value) => {
     const newInput = value.slice(-1);
-    setInput("");
-    setInput(newInput);
+    if (regex.test(newInput)) {
+      setInput("");
+      setInput(newInput);
+    } else {
+      setError(true);
+      setTimeout(() => {
+        setError(false);
+      }, 3000)
+    }
   };
 
   const handleFormSubmit = (event) => {
@@ -23,7 +34,6 @@ const TextInput = ({setInput, input, onSubmit}) => {
     <StyledTextInput>
       <form onSubmit={() => handleFormSubmit(event)}>
       <input placeholder="guess a letter" type="text" value={input} onChange={(event) => handleSetInput(event.target.value)} />
-      {/* <Button message="Ready?" onClick={onSubmit} /> */}
       </form>
     </StyledTextInput>
   )
