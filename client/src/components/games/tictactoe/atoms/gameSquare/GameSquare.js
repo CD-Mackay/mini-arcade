dc2d71dc390e1e24685bc7f0f159c9974cbce1f9
Connor Mackay <connor.mackay@gmail.com>
2022-01-27 09:59:02 -0500

    Remove indexes from gamesquares

--- client/src/components/games/tictactoe/atoms/gameSquare/GameSquare.js
+++ client/src/components/games/tictactoe/atoms/gameSquare/GameSquare.js
@@ -22,7 +22,6 @@ const GameSquare = ({index, setSquareSelected, value}) => {
   return (
     <StyledGameSquare id={`game-square-${index}`} onClick={handleSquareSelect} opacity={opacity}>
       {value && <p>{value === 1 ? "x" : "o"}</p>}
-      <h5>{index}</h5>
     </StyledGameSquare>
   )
 };
