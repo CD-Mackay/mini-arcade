375949910a2d933a60610b03dea8f59c5fc8e914
Connor Mackay <connor.mackay@gmail.com>
2022-02-07 15:35:24 -0500

    build connect four component

---
+++ client/src/components/games/ConnectFour/ConnectFour.js
@@ -0,0 +1,18 @@
+/* Library Imports */
+import React from 'react';
+
+
+/* Component Imports */
+
+/* Asset Imports */
+import StyledConnectFour from './StyledConnectFour';
+
+const ConnectFour = () => {
+  return (
+    <StyledConnectFour>
+      <h1>I am ConnectFour</h1>
+    </StyledConnectFour>
+  )
+};
+
+export default ConnectFour;
