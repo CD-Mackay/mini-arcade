d809c3fdddc7005e809cf713c84026c1e0b4bd1b
Connor Mackay <connor.mackay@gmail.com>
2022-02-11 20:05:52 -0500

    style tutorial component

--- client/src/components/atoms/Tutorial/StyledTutorial.js
+++ client/src/components/atoms/Tutorial/StyledTutorial.js
@@ -6,10 +6,13 @@ const StyledTutorial = styled.section`
 max-width: 1200px;
 border: 1px dotted #00FFFF;
 margin: 0 auto;
+padding: 10px;
 div {
   padding: 10px;
   h4 {
     font-size: 28px;
+    text-decoration: underline;
+    margin-bottom: 5px;
   }
   p {
     font-size: 20px;
