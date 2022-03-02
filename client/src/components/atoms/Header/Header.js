dba88d6e0d748a4a6c425d7889bca14c38749472
Connor Mackay <connor.mackay@gmail.com>
2022-03-02 14:32:42 -0500

    add conditional rendering to header

--- client/src/components/atoms/Header/Header.js
+++ client/src/components/atoms/Header/Header.js
@@ -4,22 +4,26 @@ import React from 'react';
 
 /* Component Imports */
 import Button from '../Button/Button';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 /* Asset Imports */
 import StyledHeader from './StyledHeader';
 
 const Header = () => {
 
-  
+  let location = useLocation();
+
+  console.log(location);
+
   return (
-    <StyledHeader>
-      <div />
+    <StyledHeader location={location.pathname}>
+     {location.pathname !== "/" && <div />}
         <h1>The Mini Arcade</h1>
+      {location.pathname !== "/" && 
       <div id="button-wrapper">
         <Link to="/">
           <Button message="Return to Home" />
         </Link>
-      </div>
+      </div>}
     </StyledHeader>
   )
 };
