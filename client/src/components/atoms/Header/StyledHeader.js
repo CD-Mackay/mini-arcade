9d1640eeadf601d2c2bb456773e9a82d4deabaf8
Connor Mackay <connor.mackay@gmail.com>
2022-03-24 22:01:46 -0400

    refactor header layout

--- client/src/components/atoms/Header/StyledHeader.js
+++ client/src/components/atoms/Header/StyledHeader.js
@@ -4,18 +4,22 @@ const StyledHeader = styled.div`
 display: grid;
 grid-template-columns: ${props => props.location === "/" ? "1fr" : "1fr 1fr 1fr"};
 padding: 10px;
-width: 100vw;
+width: 70vw;
+margin: 0 auto;
 text-align: center;
 h1 {
   font-size: 42px;
   padding: 10px;
 }
-#button-wrapper {
+
+.button-wrapper {
   display: flex;
   flex-direction: column;
   justify-content: center;
-  width: 100%;
   Button {
+    margin: 0 auto;
+  }
+  a {
     margin: 0 auto;
   }
 }
