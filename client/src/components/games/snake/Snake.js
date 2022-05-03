70c6d14e68e413d1566912a3f158d6f5686a340f
Connor Mackay <connor.mackay@gmail.com>
2022-05-03 19:54:30 -0400

    build snake componet

---
+++ client/src/components/games/snake/Snake.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import StyledSnake from "./StyledSnake";
+
+const Snake = () => {
+
+  return (
+    <StyledSnake >
+      I am Snake
+    </StyledSnake>
+  );
+};
+
+export default Snake;
---
+++ client/src/components/games/snake/StyledSnake.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledSnake = styled.section`
+
+`
+
+export default StyledSnake;
