0853097ec1be24943a5ae253e98b9f644155ea8d
Connor Mackay <connor.mackay@gmail.com>
2022-05-03 20:07:28 -0400

    implement buildboard function, style grid

--- client/src/components/games/snake/atoms/snakegrid/SnakeGrid.js
+++ client/src/components/games/snake/atoms/snakegrid/SnakeGrid.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import StyledSnakeGrid from "./StyledSnakeGrid";
 
 const SnakeGrid = () => {
 
+  const [board, setBoard] = useState();
+
+  const buildBoard = () => {
+    let board = [];
+    for (let i = 0; i < 10; i++) {
+      for (let x = 0; x < 10; x++) {
+        board.push({row: i, column: x});
+      }
+    }
+    return board.map(element => {
+      return <div className="game-square" key={[element.row, element.column]}>{element.row}, {element.column}</div>
+    })
+  }
   return (
     <StyledSnakeGrid >
-      I am SnakeGrid
+      {buildBoard()}
     </StyledSnakeGrid>
   );
 };
--- client/src/components/games/snake/atoms/snakegrid/StyledSnakeGrid.js
+++ client/src/components/games/snake/atoms/snakegrid/StyledSnakeGrid.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
 const StyledSnakeGrid = styled.div`
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr; 
+.game-square {
+  width: 100%;
+  height: 80px;
+  border: 1px solid #00FFFF;
+}
 
 `
 
