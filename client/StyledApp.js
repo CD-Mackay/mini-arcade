1ba5fb5631cfac0fdb37931113c48ee7b40348e7
Connor Mackay <connor.mackay@gmail.com>
2022-05-12 17:09:17 -0400

    add global fonts

--- client/StyledApp.js
+++ client/StyledApp.js
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
 const StyledApp = styled.section`
+
+@import url('https://fonts.googleapis.com/css2?family=Khand:wght@500&family=Koulen&family=Roboto+Slab&display=swap');
 background: #24282f;
 color: #00FFFF;
 height: 100vw;
+h1, h2, h3, h4, h5, h6 {
+  font-family: 'Khand', sans-serif;
+}
+p {
+  font-family: 'Roboto Slab', serif;
+}
 `
 
 export default StyledApp;
