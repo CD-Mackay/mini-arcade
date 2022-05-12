81aa7b3e6daa87e278c903089225961a025ffd2c
Connor Mackay <connor.mackay@gmail.com>
2022-05-12 17:20:22 -0400

    add icons with links to github and linkedin

--- client/src/components/atoms/Footer/StyledFooter.js
+++ client/src/components/atoms/Footer/StyledFooter.js
@@ -3,10 +3,17 @@ import styled from 'styled-components';
 const StyledFooter = styled.div`
 display: flex;
 justify-content: space-between;
-width: 100vw;
+width: 99vw;
 position: fixed;
 left: 0;
 bottom: 0;
+p {
+  font-size: 24px;
+  margin-left: 5px;
+}
+ul {
+  display: flex;
+}
 `
 
 export default StyledFooter;
