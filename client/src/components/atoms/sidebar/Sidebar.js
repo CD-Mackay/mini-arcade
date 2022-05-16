bc02a6da570f5e4a0af60f06075e816147d3c9eb
Connor Mackay <connor.mackay@gmail.com>
2022-05-16 14:48:54 -0400

    style links

--- client/src/components/atoms/sidebar/Sidebar.js
+++ client/src/components/atoms/sidebar/Sidebar.js
@@ -1,27 +1,32 @@
 /* Library Imports */
-import React from 'react';
-
+import React from "react";
 
 /* Component Imports */
-import TutorialText from '../../../utilities/TutorialText';
+import TutorialText from "../../../utilities/TutorialText";
+import Button from '../Button/Button';
 /* Asset Imports */
-import StyledSidebar from './StyledSidebar';
-import { Link, useLocation } from 'react-router-dom';
-
+import StyledSidebar from "./StyledSidebar";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
-
   const location = useLocation();
 
   const { text } = TutorialText;
 
   return (
     <StyledSidebar show={location.pathname}>
-      {location.pathname !== "/" && text.map((element, index) => {
-        return <Link key={index} to={`/element.game`}>{element.game}</Link>
-      })}
+      {location.pathname !== "/" &&
+        text
+          .filter((element) => !location.pathname.includes(element.game))
+          .map((element, index) => {
+            return (
+              <Link key={index} to={`/element.game`}>
+                <Button message={element.game} />
+              </Link>
+            );
+          })}
     </StyledSidebar>
-  )
+  );
 };
 
 export default Sidebar;
--- client/src/components/atoms/sidebar/StyledSidebar.js
+++ client/src/components/atoms/sidebar/StyledSidebar.js
@@ -6,7 +6,13 @@ left: 0;
 top: 0;
 height: 100vh;
 border-right: 1px solid #00FFFF;
-width: ${props => props.show === "/" ? "0px" : "100px"};
+width: ${props => props.show === "/" ? "0px" : "140px"};
+display: flex; 
+flex-direction: column;
+padding-top: 50px;
+Button {
+  width: 100%;
+}
 `
 
 export default StyledSidebar;
