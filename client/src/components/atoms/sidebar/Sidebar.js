5ea699f47d794d8d468279112b3a18d1b2e4c9a5
Connor Mackay <connor.mackay@gmail.com>
2022-05-16 14:45:52 -0400

    add dynamic links to sidebar

--- client/src/components/atoms/sidebar/Sidebar.js
+++ client/src/components/atoms/sidebar/Sidebar.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 
 /* Component Imports */
+import TutorialText from '../../../utilities/TutorialText';
 /* Asset Imports */
 import StyledSidebar from './StyledSidebar';
 import { Link, useLocation } from 'react-router-dom';
@@ -11,11 +12,14 @@ import { Link, useLocation } from 'react-router-dom';
 const Sidebar = () => {
 
   const location = useLocation();
-  console.log(location);
+
+  const { text } = TutorialText;
 
   return (
     <StyledSidebar show={location.pathname}>
-
+      {location.pathname !== "/" && text.map((element, index) => {
+        return <Link key={index} to={`/element.game`}>{element.game}</Link>
+      })}
     </StyledSidebar>
   )
 };
