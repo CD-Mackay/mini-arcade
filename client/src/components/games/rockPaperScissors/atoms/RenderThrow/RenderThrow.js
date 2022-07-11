939e5747daea40cd2db55c15f767f11477188cb9
Connor Mackay <connor.mackay@gmail.com>
2022-07-11 16:33:29 -0400

    Add text content to rockpaper scissors move display

--- client/src/components/games/rockPaperScissors/atoms/RenderThrow/RenderThrow.js
+++ client/src/components/games/rockPaperScissors/atoms/RenderThrow/RenderThrow.js
@@ -5,14 +5,42 @@ import { FaRegHandScissors } from "react-icons/fa";
 import StyledRenderThrow from "./StyledRenderThrow";
 
 const RenderThrow = ({ move }) => {
+  // const shownThrow = (input) => {
+  //   let icon = null;
+  //   input === "rock" ?
+  //   icon = <GiMailedFist fontSize="36px" id="rock" />
+  //   : input === "paper" ? icon = <GiPalm fontSize="36px" id="paper" />
+  //   : input === "scissors" ? icon = <FaRegHandScissors fontSize="36px" id="scissors" /> : icon = <GiMailedFist fontSize="36px" color="#24282f" />
+  //   return icon;
+  // };
+
   return (
     <StyledRenderThrow move={move}>
       <h1>Your Selection:</h1>
-      {!move && <GiMailedFist fontSize="36px" color="#24282f" />}
-      {move === "rock" && <GiMailedFist fontSize="36px" id="rock" />}
-      {move === "paper" && <GiPalm fontSize="36px" id="paper" />}
+      {() => shownThrow(move)}
+      {!move && (
+        <div className="icon-display">
+          <GiMailedFist fontSize="36px" color="#24282f" />
+          <p>{move}</p>
+        </div>
+      )}
+      {move === "rock" && (
+        <div className="icon-display">
+          <GiMailedFist fontSize="36px" id="rock" />
+          <p>{move}</p>
+        </div>
+      )}
+      {move === "paper" && (
+        <div className="icon-display">
+          <GiPalm fontSize="36px" id="paper" />
+          <p>{move}</p>
+        </div>
+      )}
       {move === "scissors" && (
-        <FaRegHandScissors fontSize="36px" id="scissors" />
+        <div className="icon-display">
+          <FaRegHandScissors fontSize="36px" id="scissors" />
+          <p>{move}</p>
+        </div>
       )}
     </StyledRenderThrow>
   );
