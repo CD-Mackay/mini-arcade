d44d575a25db51b315c1b6fd8c06d767b616e59e
Connor Mackay <connor.mackay@gmail.com>
2022-07-12 13:57:58 -0400

    style dropdown

--- client/src/components/atoms/Dropdown/StyledDropdown.js
+++ client/src/components/atoms/Dropdown/StyledDropdown.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
 
 const StyledDropdown = styled.div`
+.drop {
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
 
 `
 
