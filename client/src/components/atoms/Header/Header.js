83a8ed56d4694f50060d6ffe9b4e965ae88c0d2d
Connor Mackay <connor.mackay@gmail.com>
2022-07-13 18:45:05 -0400

    remove unused components from header

--- client/src/components/atoms/Header/Header.js
+++ client/src/components/atoms/Header/Header.js
@@ -7,6 +7,7 @@ import { Link, useLocation } from "react-router-dom";
 import StyledHeader from "./StyledHeader";
 import { BsHouseDoor } from "react-icons/bs";
 import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { FaQuestion } from "react-icons/fa";
 import Dropdown from "../Dropdown/Dropdown";
 
 const Header = () => {
@@ -24,33 +25,17 @@ const Header = () => {
             <Dropdown />
           </div>
           <div className="button-wrapper">
-            <Link to={`${location.pathname}/tutorial`}>
-              <AiOutlineQuestionCircle color="#00FFFF" fontSize="28px" />
-            </Link>
-          </div>
-
-          <div className="button-wrapper">
             <Link to="/">
               <BsHouseDoor color="#00FFFF" fontSize="24px" />
             </Link>
           </div>
+          <div className="button-wrapper">
+            <Link to={`${location.pathname}/tutorial`}>
+              <FaQuestion color="#00FFFF" fontSize="22px" />
+            </Link>
+          </div>
         </div>
       )}
-      {/* {location.pathname !== "/" && (
-        <div className="button-wrapper">
-        <Link to={`${location.pathname}/tutorial`} >
-          <AiOutlineQuestionCircle color="#00FFFF" fontSize="28px" />
-        </Link>
-        </div>
-      )}
-      <h1>The Mini Arcade</h1>
-      {location.pathname !== "/" && (
-        <div className="button-wrapper">
-          <Link to="/">
-            <BsHouseDoor color="#00FFFF" fontSize="24px" />
-          </Link>
-        </div>
-      )} */}
     </StyledHeader>
   );
 };
