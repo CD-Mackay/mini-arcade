c0e686bc540f95075e51e6fbd6eeb215874a072a
Connor Mackay <connor.mackay@gmail.com>
2022-07-27 14:56:50 -0400

    fix resizing issue on mobile

--- client/src/components/atoms/Header/StyledHeader.js
+++ client/src/components/atoms/Header/StyledHeader.js
@@ -1,57 +1,53 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const StyledHeader = styled.div`
-display: flex;
-justify-content: ${props => props.location === "/" ? "center" : "space-between"};
-width: 100vw;
-margin: 0 auto;
-text-align: center;
-h1 {
-  font-size: 42px;
-  padding: 10px;
-}
-
-.nav {
   display: flex;
-  gap: 10px;
-}
-
-.drop-wrapper {
-  visibility: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  Button {
-    margin: 0 auto;
+  justify-content: ${(props) =>
+    props.location === "/" ? "center" : "space-between"};
+  width: 100vw;
+  margin: 0 auto;
+  text-align: center;
+  h1 {
+    font-size: 42px;
+    padding: 10px;
   }
-  a {
-    margin: 0 auto;
-  }
-}
 
-
-.button-wrapper {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  Button {
-    margin: 0 auto;
+  .nav {
+    display: flex;
+    flex-shrink: 3;
+    gap: 10px;
   }
-  a {
-    margin: 0 auto;
-  }
-}
 
-@media (min-width: 576px) {
-  width: 90vw;
   .drop-wrapper {
-    visibility: visible;
+    display: none;
+    Button {
+      margin: 0 auto;
+    }
+    a {
+      margin: 0 auto;
+    }
   }
-}
 
-@media (min-width: 992px) {
-  width: 70vw;
-}
-`
+  .button-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    Button {
+      margin: 0 auto;
+    }
+    a {
+      margin: 0 auto;
+    }
+  }
+
+  @media (min-width: 576px) {
+    width: 90vw;
+    .drop-wrapper {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+    }
+  }
+`;
 
 export default StyledHeader;
