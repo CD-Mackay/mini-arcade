f0951f2110e848db2fe6e510a2e8bc7c1e926083
Connor Mackay <connor.mackay@gmail.com>
2022-07-27 15:09:46 -0400

    convert gamelist to use mapped data

--- client/src/components/atoms/GameList/GameList.js
+++ client/src/components/atoms/GameList/GameList.js
@@ -1,53 +1,66 @@
 /* Library Imports */
-import React from 'react';
-
+import React from "react";
 
 /* Component Imports */
-import GameListItem from '../GameListItem/GameListItem';
+import GameListItem from "../GameListItem/GameListItem";
 
 /* Asset Imports */
-import StyledGameList from './StyledGameList';
-import { GiTicTacToe, GiDoubleFaceMask, GiTriforce } from 'react-icons/gi';
-import { MdConstruction } from 'react-icons/md';
-import { BiCategory } from 'react-icons/bi';
+import StyledGameList from "./StyledGameList";
+import { GiTicTacToe, GiDoubleFaceMask, GiTriforce } from "react-icons/gi";
+import { MdConstruction } from "react-icons/md";
+import { BiCategory } from "react-icons/bi";
 
 const GameList = () => {
+  const GAMES = [
+    {
+      game: "tictactoe",
+      name: "Tic-Tac-Toe",
+      description: "Simple, classic, your child can probably beat you.",
+      icon: <GiTicTacToe color={"#00FFFF"} fontSize="48px" />,
+    },
+    {
+      game: "hangman",
+      name: "Hangman",
+      description: "Slightly morbid, good for vocabulistics.",
+      icon: <GiDoubleFaceMask color={"#00FFFF"} fontSize="48px" />,
+    },
+    {
+      game: "rockpaperscissors",
+      name: "Rock Paper Scissors",
+      description: "For those who can't do",
+      icon: <GiTriforce color={"#00FFFF"} fontSize="48px" />,
+    },
+    {
+      game: "connectfour",
+      name: "Connect Four",
+      description: "Because counting to 5 is hard.",
+      icon: <BiCategory color={"#00FFFF"} fontSize="48px" />,
+    },
+    {
+      game: "snake",
+      name: "Snake",
+      description: "it's a snaaake (in development).",
+      icon: <MdConstruction color={"#00FFFF"} fontSize="48px" />,
+    },
+  ];
   return (
     <StyledGameList>
       <div className="game-wrapper">
-        <GameListItem 
-        game="tictactoe" 
-        name="Tic-Tac-Toe"
-        description="Simple, classic, your child can probably beat you. "
-        icon={<GiTicTacToe color={"#00FFFF"} fontSize="48px" />} />
-        <GameListItem 
-        game="hangman" 
-        name="Hangman" 
-        description="Slightly morbid, good for vocabulistics"
-        icon={<GiDoubleFaceMask color={"#00FFFF"} fontSize="48px" />}
-        />
-        <GameListItem 
-        game="rockpaperscissors" 
-        name="Rock Paper Scissors" 
-        description="For those who can't do."
-        icon={<GiTriforce color={"#00FFFF"} fontSize="48px" />}
-        />
-        <GameListItem 
-        game="connectfour" 
-        name="Connect Four" 
-        description="Because counting to 5 is hard."
-        icon={<BiCategory color={"#00FFFF"} fontSize="48px" />}
-         />
-        <GameListItem 
-        game="snake" 
-        name="Snake" 
-        description="it's a snaaake (in development)"
-        icon={<MdConstruction color={"#00FFFF"} fontSize="48px" />}
-        />
-        <GameListItem/>
-    </div>
+        {GAMES.map((element, index) => {
+          return (
+            <GameListItem
+              game={element.game}
+              name={element.name}
+              description={element.description}
+              icon={element.icon}
+              key={element.index}
+            />
+          );
+        })}
+        <GameListItem />
+      </div>
     </StyledGameList>
-  )
+  );
 };
 
 export default GameList;
