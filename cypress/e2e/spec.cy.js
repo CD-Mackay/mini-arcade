95cc5e4dda77f3e730ab33969403e80ffdc52bf6
Connor Mackay <connor.mackay@gmail.com>
2022-08-08 16:21:41 -0400

    TST: Implement initial App tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('App Test', () => {
+  it('Opens the App and renders the home page', () => {
+    cy.visit('http://localhost:3001')
+    cy.contains('The Mini Arcade')
+  })
+
+  it('Opens a game of Tic-Tac-Toe', () => {
+    cy.visit('http://localhost:3001')
+    cy.contains('Tic-Tac-Toe').click()
+    cy.url().should('include', '/tictactoe')
+
+    cy.contains('Human').click()
+    cy.contains('currently Player')
+
   })
 })
