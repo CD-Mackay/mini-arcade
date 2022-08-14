2cd8d0d591d0de6a4ff46f836fc6fea27f2d18bb
Connor Mackay <connor.mackay@gmail.com>
2022-08-13 20:32:05 -0400

    organize code for Tic-Tac-Toe

--- client/src/components/games/tictactoe/atoms/gameSquare/StyledGameSquare.js
+++ client/src/components/games/tictactoe/atoms/gameSquare/StyledGameSquare.js
@@ -2,15 +2,15 @@ import styled from 'styled-components';
 
 const StyledGameSquare = styled.div`
 border: 1px solid #00FFFF;
-height: 50px;
-width: 100%;
 display: flex;
 flex-direction: column;
+height: 50px;
 justify-content: center;
+width: 100%;
   p {
     font-size: 46px;
-    transition: opacity 0.3s ease-out;
     opacity: ${props => props.opacity == 0 ? 0 : 1};
+    transition: opacity 0.3s ease-out;
   }
 :hover {
   background: #8d1b1f;
