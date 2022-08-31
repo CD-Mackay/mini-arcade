0678864b48d76c25ef375ca1fc69a7df5d76aea6
Connor Mackay <connor.mackay@gmail.com>
2022-08-31 19:04:08 -0400

    TST: Update Connect four Tests

--- cypress/e2e/connect_four.spec.cy.js
+++ cypress/e2e/connect_four.spec.cy.js
@@ -21,4 +21,40 @@ describe('Connect Four', () => {
 
     cy.contains('Square not available')
   })
+
+  it('Correctly displays the winner of the game', () => {
+    cy.visit('http://localhost:3001/connectfour')
+    cy.get('.57').click()
+    cy.get('.56').click()
+    cy.get('.47').click()
+    cy.get('.37').click()
+    cy.get('.46').click()
+    cy.get('.36').click()
+    cy.get('.35').click()
+    cy.get('.26').click()
+    cy.get('.55').click()
+    cy.get('.54').click()
+    cy.get('.45').click()
+    cy.get('.35').click()
+    cy.get('.45').click()
+    cy.get('.27').click()
+    cy.get('.53').click()
+    cy.get('.17').click()
+    cy.get('.44').click()
+
+    cy.contains('Player 1 is victorious!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })
 })
