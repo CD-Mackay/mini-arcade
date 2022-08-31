5617a1f41498fcb5b8564964cb028b49a59d370b
Connor Mackay <connor.mackay@gmail.com>
2022-08-31 18:52:30 -0400

    TST:Update hangman tests

--- cypress/e2e/hangman_spec.cy.js
+++ cypress/e2e/hangman_spec.cy.js
@@ -34,4 +34,39 @@ describe('Hangman', () => {
     cy.get('input').type('z{enter}')
     cy.contains('z')
   })
+
+  it('Renders the victory message upon success', () => {
+    cy.visit('http://localhost:3001/hangman')
+    cy.contains('Play Against Human').click()
+    cy.get('input').type('cypress')
+
+    cy.contains('Start Game').click()
+    cy.get('input').type('s{enter}')
+    cy.get('input').type('e{enter}')
+    cy.get('input').type('r{enter}')
+    cy.get('input').type('c{enter}')
+    cy.get('input').type('y{enter}')
+    cy.get('input').type('p{enter}')
+
+    cy.contains('Congratulations! You were not hanged')
+  })
+
+  it('Renders the loss message', () => {
+    cy.visit('http://localhost:3001/hangman')
+    cy.contains('Play Against Human').click()
+    cy.get('input').type('cypress')
+
+    cy.contains('Start Game').click()
+
+    cy.get('input').type('z{enter}')
+    cy.get('input').type('w{enter}')
+    cy.get('input').type('q{enter}')
+    cy.get('input').type('n{enter}')
+    cy.get('input').type('v{enter}')
+    cy.get('input').type('o{enter}')
+
+    cy.contains('You have been hanged')
+
+
+  })
 })
